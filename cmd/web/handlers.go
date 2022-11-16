443524d8a1761a2be6a69f1396071db4c6595d1e
jbresky <joaquin_bresky@hotmail.com>
2022-11-16 11:29:36 -0300

    serving static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,19 +13,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/home.page.html")
+
+	files := []string{
+		"./ui/html/home.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server Error", 500)
 		return
 	}
+
 	err = ts.Execute(w, nil) // this last parameter represents dynamic data. for now it's just nil
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server Error", 500)
 		return
 	}
-	// w.Write([]byte("Hello from Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
